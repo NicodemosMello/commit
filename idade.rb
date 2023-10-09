fde25ab092e49e9e521859bd7d1f2ba5e6d1e69c
nicodemosmello <nicodemusgimello21@gmail.com>
2023-10-09 12:55:52 -0300

    arquivo idade.rb alterado

--- idade.rb
+++ idade.rb
@@ -1,21 +1,20 @@
-=begin algoritmo "Soma das idades"
-// Funçãoo : Calcular idade do usuário
-// Autor : Eu
-// Data : 08/10/2023
-=end
+# Solicita o nome e sobrenome do usuário
+print 'Digite seu nome: '
+nome = gets.chomp
 
-# variáveis 
-   nome = "caracter"
-   data_nascimento = "data"
-   data_atual = "data"
-   soma = "inteiro"
+# Solicita ao usuário que informe a data do seu nascimento
+print 'Digite sua data de nascimento no formato (DD/MM/AAAA): '
+data_nascimento = gets.chomp
 
-# inicio
-   print 'Digite sua data de nascimento: '
-   data_nascimento = gets.chomp.to_i
-   print 'Digite a data atual: '
-   data_atual = gets.chomp.to_i
-   subtract = data_atual - data_nascimento
+#Converte a data de nasciemnto em um objeto Date do Ruby
+require 'date'
+data_nascimento = Date.parse(data_nascimento)
 
-# retorne a idade para usuário
-   puts "Sua idade é #{subtract}"     
+#Calcula a data atual
+data_atual = Date.today
+
+# Calcula a idade do usuário
+idade = data_atual.year - data_nascimento.year - ((data_atual.month > data_nascimento.month || (data_atual.month == data_nascimento.month && data_atual.day >= data_nascimento.day)) ? 0 : 1)
+
+# Exibe a idade do usuário
+puts "Olá #{nome}, sua idade é: #{idade} anos"
